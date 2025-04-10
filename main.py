233fab03527c5b39cb8cf816d8d92abdc99238e9
Akhil Datla <66145155+akhil-datla@users.noreply.github.com>
2025-04-10 12:46:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-import math
-import numpy as np
 import pandas as pd
-from algorithm import Matching, Group
 import sys
 
-# Set maximum number of students in a room
-groupSize = 4
+# Default group size is 4
+groupSize = int(input("What is the maximum number of students that can be in 1 room?: "))
+
+if groupSize <= 0:
+  groupSize = 4
+
+# Allowed max difference in grade levels
+allowedDiff = int(input("What is the maximum allowed difference in grade levels between students in a room?: "))
 
 #Student Groups list
 studentGroups = []
 
+filePath = input("Please enter the name of the CSV file with the students' preferences: ")
+
 # Check if there is a CSV file with the list of students and their preferences
-if len(sys.argv) < 2:
+if len(file) < 1:
     print("Please input a CSV file with students and their preferences.")
     sys.exit()
 else:
     # Import the list of students as a Pandas dataframe
-    studentsDf = pd.read_csv(sys.argv[1], header=0)
+    studentsDf = pd.read_csv(filePath, header=0)
 
 if studentsDf.shape[0] <= 0:
     print("Dataframe is empty.")
@@ -38,21 +43,27 @@ for idx in studentsDf.index:
     ]
 
     for pref in prefs:
-        
         # check if the prefrence is in the dataset
         if (studentsDf[studentsDf["Email Address"] == pref].index.values.size <= 0):
             errorDf.loc[len(errorDf.index)] = [row["Full Name"], student, "Preference not in dataset or does not have preference"]
-            studentsDf.drop(idx, inplace=True)
             continue
-            
+
         # check if student put themself as a preference
         elif (student == pref):
             errorDf.loc[len(errorDf.index)] = [row["Full Name"], student, "Preference is themself"]
-            studentsDf.drop(idx, inplace=True)
             continue
-            
-        else:
 
+        # check if student and their preference are the same gender
+        elif (row["Gender"] != studentsDf[studentsDf["Email Address"] == pref]["Gender"].values[0]):
+            errorDf.loc[len(errorDf.index)] = [row["Full Name"], student, "Preference is not the same gender"]
+            continue
+
+        # check if the grade levels of the student and preference are more than one apart
+        elif (abs(row["Grade"] - studentsDf[studentsDf["Email Address"] == pref]["Grade"].values[0]) > allowedDiff):
+            errorDf.loc[len(errorDf.index)] = [row["Full Name"], student, "Preference is more than one grade level apart"]
+            continue
+
+        else:
             studentPrefMatrix.itemset(
                 (
                     studentsDf[studentsDf["Email Address"] == pref].index[0],
